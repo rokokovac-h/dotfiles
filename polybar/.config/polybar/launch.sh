4d17ee7fc9554445f26f1fe429dbda2c41119b87
Roko Kovac <rokokovac98@gmail.com>
2025-06-18 20:49:24 +0200

    add 2 monitor config

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,10 +1,13 @@
 killall -q polybar
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch polybar on each connected monitor
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload toph &
-  done
-else
-  polybar --reload toph &
-fi
+# Launch on primary first so the system tray stays on it 
+primary=$(xrandr --query | grep " primary " | cut -d" " -f1)
+MONITOR=$primary polybar --reload toph &
+
+# Launch on all connected secondary monitors
+secondary=$(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1)
+for m in $secondary; do
+  MONITOR=$m polybar --reload toph &
+done
+
